b555e8c40d2fe3cf0b10871a6ff9d50ed1a6f650
shino0928 <52171191+shino0928@users.noreply.github.com>
2019-07-02 02:10:55 +0900

    最初のテスト
    はじめてのコミット

---
+++ test.js
@@ -0,0 +1,22 @@
+
+
+
+var requestURL = 'JSON.html';
+//取得したい JSON がある URL を変数へ代入します。次のコードを JavaScript の最後の行へ追加してください。
+
+
+var request = new XMLHttpRequest();
+//HTTP リクエストを作成するのに、new を使って XMLHttpRequest から新しいリクエストオブジェクトをつくる必要があります。先ほどのコードの下に、次のコードを追加してください。
+
+
+request.open('GET', requestURL);
+//新しいリクエストを開始するのには open() メソッドを使います。 次のコードを追加してください。
+
+
+request.responseType = 'json';
+request.send();
+
+var til_db_json = request.response;
+
+document.write(json.name);
+
